f042df5cbc3a1ca8e3b63a8bc8586715e43c2251
Khan-Taslim <khantaslim305@gmail.com>
2023-03-27 12:59:36 +0530

    sid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { HomeComponent } from './home/home.component';
@@ -62,7 +62,8 @@ import { CalendarModule } from '@syncfusion/ej2-angular-calendars';
    FormsModule,
    ReactiveFormsModule,
    NgApexchartsModule,
-   CalendarModule
+   CalendarModule,
+   MatIconModule
     
   ],
   providers: [],
